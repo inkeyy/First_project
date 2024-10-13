002a4476675aabfb87d3923737ca3d59f07e51cf
USER-PC10 <mrshishikina@gmail.com>
2024-10-13 18:45:52 +0300

    Добавьте файлы проекта.

---
+++ First_project/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("ТЕКСТ: ");
+string t =  Console.ReadLine();
+Console.WriteLine("колво символов - " + t.Length);
+Console.WriteLine("вверхний рег - " + t.ToUpper());
+Console.WriteLine("нижний рег - " + t.ToLower());
+Console.WriteLine("замена - " + t.Replace(" ", "_"));
